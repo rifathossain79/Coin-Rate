c0ff107016f6f83fb6154147fedb2f915ce8d311
rifathossain79 <126547462+rifathossain79@users.noreply.github.com>
2023-04-06 22:11:41 +0600

    Project Commit

---
+++ src/controller/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../view/Banner/Banner";
+import CoinsTable from "../view/CoinsTable";
+
+const Homepage = () => {
+  return (
+    <>
+      <Banner />
+      <CoinsTable />
+    </>
+  );
+};
+
+export default Homepage;
---
+++ src/model/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZvKt5_VvwquR0HMHsJQOpLzgsqdpDQXo",
+    authDomain: "coin-check-57543.firebaseapp.com",
+    projectId: "coin-check-57543",
+    storageBucket: "coin-check-57543.appspot.com",
+    messagingSenderId: "160316745106",
+    appId: "1:160316745106:web:0412d828b7ba1e3cad4d4b",
+  };
+  
+
+  export default firebaseConfig;
