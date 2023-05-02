c26fa495c3e2517610cc6895fa86c1ca76eac255
rifathossain79 <126547462+rifathossain79@users.noreply.github.com>
2023-05-02 22:08:51 +0600

    Update firebaseConfig.js

--- src/model/firebaseConfig.js
+++ src/model/firebaseConfig.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBZvKt5_VvwquR0HMHsJQOpLzgsqdpDQXo",
-    authDomain: "coin-check-57543.firebaseapp.com",
-    projectId: "coin-check-57543",
-    storageBucket: "coin-check-57543.appspot.com",
-    messagingSenderId: "160316745106",
-    appId: "1:160316745106:web:0412d828b7ba1e3cad4d4b",
-  };
+  apiKey: "AIzaSyBAVZ72tveNkumZU0zBx1cotZyJDlPVgP4",
+  authDomain: "coin-check-rifat.firebaseapp.com",
+  projectId: "coin-check-rifat",
+  storageBucket: "coin-check-rifat.appspot.com",
+  messagingSenderId: "260536051630",
+  appId: "1:260536051630:web:a8490194f892fe6bd78a3f"
+};
+
   
 
   export default firebaseConfig;
